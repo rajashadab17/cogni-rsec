7cd7eb7b0e579026e3672f9bc52752cf2e9f1b90
rajashadab17 <rajashadab1711@gmail.com>
2025-12-06 22:27:19 +0500

    fix(error): remove hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   const PathNameArray = pathname.split("/").filter(Boolean);
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
