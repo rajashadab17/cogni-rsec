21b78a0f15dc1605c2705c8e4950328388f83d80
rajashadab17 <rajashadab1711@gmail.com>
2025-12-06 15:21:13 +0500

    Integrated next-themes for different color modes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster position="top-right" closeButton />
-        {children}
+        <main>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toaster position="top-right" closeButton />
+            {
+              children
+            }
+          </ThemeProvider>
+        </main>
       </body>
     </html>
   );
