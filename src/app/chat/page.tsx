decbdb26d1f5e23393b34016425e99b7bf886d55
rajashadab17 <rajashadab1711@gmail.com>
2025-12-07 09:52:15 +0500

    Developed header in chat page

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div className="flex h-auto w-full bg-white dark:bg-gray-900">
+        <div className="flex-1 flex flex-col max-w-4xl mx-auto w-full">
+          <div className="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 p-4">
+            <div className="text-center">
+              <h1 className="text-xl font-medium text-gray-900 dark:text-white">
+                How can I help you?
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
