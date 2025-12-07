2dd9dba3b8ff5550025df6f1f2cf7cee7d7d25f7
rajashadab17 <rajashadab1711@gmail.com>
2025-12-07 11:11:21 +0500

    fix formatting in main layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,55 +60,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             {showSidebar ? (
               <SidebarProvider>
                 <AppSidebar />
-                <SidebarInset>
-                  <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                    <div className="flex items-center gap-2 px-4">
-                      <SidebarTrigger className="-ml-1" />
-                      <Separator
-                        orientation="vertical"
-                        className="mr-2 data-[orientation=vertical]:h-4"
-                      />
-                      <Breadcrumb>
-                        <BreadcrumbList>
-                          {PathNameArray.map((pathName, index) => {
-                            return (
-                              <>
-                                {index != PathNameArray.length - 1 && (
-                                  <>
-                                    <BreadcrumbItem className="hidden md:block">
-                                      <BreadcrumbLink
-                                        href="#"
-                                        className="capitalize"
-                                      >
-                                        {pathName == 'sales' ? 'Billing & Sales' : pathName}
-                                      </BreadcrumbLink>
-                                    </BreadcrumbItem>
-                                  </>
-                                )}
-                                {index != PathNameArray.length - 1 && (
-                                  <BreadcrumbSeparator className="hidden md:block" />
-                                )}
-                                {index == PathNameArray.length - 1 && (
-                                  <>
-                                    <BreadcrumbItem>
-                                      <BreadcrumbPage className="capitalize">
-                                        {pathName}
-                                      </BreadcrumbPage>
-                                    </BreadcrumbItem>
-                                  </>
-                                )}
-                              </>
-                            );
-                          })}
-                        </BreadcrumbList>
-                      </Breadcrumb>
-                    </div>
-                    {/* <div className="ml-auto px-3">
-                      <NavActions />
-                    </div> */}
-                  </header>
                   {children}
-                </SidebarInset>
               </SidebarProvider>
             ) : (
               children
